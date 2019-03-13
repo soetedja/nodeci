a58b0bf759b99397d421774b847f25deed0012f0
irfansoetedja <soetedja@gmail.com>
2019-03-13 08:05:17 +0700

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -3,7 +3,7 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI:
-    'mongodb://bowo:bowo@cluster0-shard-00-00-rythe.mongodb.net:27017,cluster0-shard-00-01-rythe.mongodb.net:27017,cluster0-shard-00-02-rythe.mongodb.net:27017/blog_dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
---
+++ tests/blogs.test.js
@@ -0,0 +1,63 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+
+  test('can see blog create form', async () => {
+    const label = await page.getContentsOf('form label');
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
+
+  describe.skip('User is not logged in', async () => {
+    const actions = [
+      {
+        method: 'get',
+        path: '/api/blogs'
+      },
+      {
+        method: 'post',
+        path: '/api/blogs',
+        data: {
+          title: 'T',
+          content: 'C'
+        }
+      }
+    ];
+
+    test('Blog related actions are prohibited', async () => {
+      const results = await page.execRequests(actions);
+      console.log(results);
+      for (let result of results) {
+        expect(result).toEqual({ error: 'You must log in!' });
+      }
+    });
+  });
+});
